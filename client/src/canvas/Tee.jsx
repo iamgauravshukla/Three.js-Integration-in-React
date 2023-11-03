a27bcb83cd3e33d28050acca294017a3aa504473
gauravshukla <gaurav.shukla@ioanyt.com>
2023-11-03 18:11:58 +0530

    Tee Fixes

--- client/src/canvas/Tee.jsx
+++ client/src/canvas/Tee.jsx
@@ -18,7 +18,28 @@ const Tee = () => {
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            anisotropy = {16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
     </group>
   );
 };
